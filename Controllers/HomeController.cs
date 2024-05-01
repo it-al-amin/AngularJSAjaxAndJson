47f223eab27a8f895c578dedd9f643c837c2ad60
Md Al Amin <al.amin@amigoinformatics.com>
2024-05-01 07:18:09 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using AngularJSAjaxAndJson.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AngularJSAjaxAndJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+       
+        public JsonResult AjaxMethod([FromBody] PersonModelcs model)
+        {
+            var person = new PersonModelcs
+            {
+                Name = model.Name,
+                DateTime = DateTime.Now.ToString()
+            };
+
+            return Json(person);
+        }
+    }
+}
---
+++ Models/PersonModelcs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularJSAjaxAndJson.Models
+{
+    public class PersonModelcs
+    {
+        public string Name { get; set; }
+        public string DateTime { get; set; }
+    }
+}
